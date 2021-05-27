522bdbea284833c90d852bdafc34f5962c2dc51b
developer deepu <65069470+sudeepdeep@users.noreply.github.com>
2021-05-27 23:06:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask , render_template, request, redirect, url_for , session
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	
+	return render_template('index.html')
+
+
+@app.route('/contact')
+def contact():
+	
+	return render_template('index2.html')
+
+
+if __name__ == '__main__':
+	app.run(debug = True)
